b78dbeb5f394b2d4d5bafaacddc0c911fc852035
SamOye95 <a.s.m.oyediran@student.utwente.nl>
2019-10-31 19:16:03 +0100

    class tree

--- classification trees.R
+++ classification trees.R
@@ -1,5 +1,4 @@
-
-
+source("dataclean.R")
 #This function trains our flexible classification tree
 # Arugements: 
 # dtm : 
@@ -7,19 +6,28 @@
 # complexity: the cost-complexity pruning parameter (called CP in Rpart) 
 # for classification trees,
 
-train.class <- function(dtm, labels, complexity){
-  
+train.class <- function(dtm, labels)
+{
   dtm.frame <- data.frame(as.matrix(dtm), label=labels)
   dtm.tree <- rpart(label~., method="class", data = dtm.frame, control = rpart.control(xval=10, minsplit=4, cp = 0, maxdepth=10))
-  
   # simple tree for plotting
-  dtm.tree.pruned <-prune(dtm.tree,cp= 1.37e-02)
+  plot(dtm.tree)
+
+  #pruned tree
+  dtm.tree.pruned <-prune(dtm.tree,cp= 0.025)
   plot(dtm.tree.pruned)
-  
-  # tree with lowest cv error
-  dtm.tree.pruned <-prune(dtm.tree,cp=0.001)
-  
-  # make predictions on the dataset
-  dtm.tree.pred <- predict(dtm.tree.pruned,newdata=dtm.frame,type= "class")
+  return (dtm.tree.pruned)
 }
 
+predict.class <- function(model, dtm)
+{
+  test.frame <- data.frame(as.matrix(dtm))
+  pred.tree <- predict(model, test.frame, type="class")
+  return(class.pred <- as.numeric(pred.tree)-1)
+}
+
+#ct_unimodel <- train.class(Unigramdftrain, trainlabels)
+ct_bimodel <- train.class(data.frame(as.matrix(Bigramdftrain)), trainlabels)
+
+#result_ct_uni <- table(testlabels, predict.class(ct_unimodel, Unigramdftest))
+result_ct_bi <- table(testlabels, predict.class(ct_bimodel, Bigramdftest))
