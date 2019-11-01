12cc4cb25ba5939f2c5444d00eec07399d675ce2
SamOye95 <a.s.m.oyediran@student.utwente.nl>
2019-11-01 21:37:28 +0100

    last commit

--- classification trees.R
+++ classification trees.R
@@ -1,3 +1,5 @@
+library(rpart.plot)		
+
 source("dataclean.R")
 #This function trains our flexible classification tree
 # Arugements: 
@@ -11,11 +13,17 @@ train.class <- function(dtm, labels)
   dtm.frame <- data.frame(as.matrix(dtm), label=labels)
   dtm.tree <- rpart(label~., method="class", data = dtm.frame, control = rpart.control(xval=10, minsplit=4, cp = 0, maxdepth=10))
   # simple tree for plotting
-  plot(dtm.tree)
-
+  dev.new(width = 550, height = 330, unit = "px")
+  #plot(dtm.tree)
+  #Visualize the decision tree with rpart.plot
+  #rpart.plot(dtm.tree, box.palette = "RdBu", shadow.col = "gray", nn=TRUE)
+  
   #pruned tree
-  dtm.tree.pruned <-prune(dtm.tree,cp= 0.025)
-  plot(dtm.tree.pruned)
+  dtm.tree.pruned <-prune(dtm.tree,cp= 0.0137)
+  #Visualize the decision tree with rpart.plot
+  rpart.plot(dtm.tree.pruned, box.palette = "RdBu", shadow.col = "gray", nn=TRUE)
+  #plot(dtm.tree.pruned)
+  
   return (dtm.tree.pruned)
 }
 
@@ -26,8 +34,14 @@ predict.class <- function(model, dtm)
   return(class.pred <- as.numeric(pred.tree)-1)
 }
 
-#ct_unimodel <- train.class(Unigramdftrain, trainlabels)
-ct_bimodel <- train.class(data.frame(as.matrix(Bigramdftrain)), trainlabels)
+#ct_unimodel <- train.class(Unigramdftrain[,row.names(data.frame( tail(sort(colSums(Unigramdftrain) ),1000)))], trainlabels)
+#ct_unimodel$cptable
+#plotcp(ct_unimodel)
+
+bidata =Bigramdftrain[row.names(data.frame(tail(sort(colSums(Bigramdftrain) ),1000)))]
+
+ct_bimodel <- train.class(data.frame(as.matrix(bidata)), trainlabels)
 
 #result_ct_uni <- table(testlabels, predict.class(ct_unimodel, Unigramdftest))
+
 result_ct_bi <- table(testlabels, predict.class(ct_bimodel, Bigramdftest))
